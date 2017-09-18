2c82e29cc212cb66061fa1d8d8048ce25f68fb6e
Kenny Skaggs <skaggskd88@gmail.com>
2017-09-18 12:10:54 -0500

    craigs changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,16 +13,9 @@ namespace LatinDictionaryNoAuth.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Manage()
         {
             ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
